2a54cebf78b9668a4e9d770c9ed3a0f97bc87b82
abdullaevv <gadzhi.abdullaev.20@mail.ru>
2022-07-28 14:23:53 +0300

    add timer

--- js/script.js
+++ js/script.js
@@ -34,4 +34,56 @@ window.addEventListener('DOMContentLoaded', function() {
         }
 
     });
+
+    //timer
+
+    let deadLine = '2022-07-29';
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/(1000*60*60)));
+            //hours = Math.floor((t/1000/60/60) % 24); //Таймер для дней 
+            //days = Math.floor((t/(1000*60*60*24)));  //Таймер для дней 
+        return {
+            'total' : t, 
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+
+            if (hours < 10) {
+                hours.textContent = '0' + t.hours;
+            } else {
+                hours.textContent = t.hours;
+            }
+            
+            //тоже самое только короче)
+            minutes.textContent = t.minutes < 10 ? '0' + t.minutes : t.minutes;
+            seconds.textContent = t.seconds < 10 ? '0' + t.seconds : t.seconds;
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+
+            }
+        }
+    }
+
+
+
+    setClock('timer', deadLine);
 });
